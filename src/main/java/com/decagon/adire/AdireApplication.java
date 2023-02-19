b10f40d23926c31135191540385338523e5122a9
Decagon <decagon@SamuelDecagon.local>
2023-02-19 08:54:32 +0100

    ER Diagram - B

--- src/main/java/com/decagon/adire/AdireApplication.java
+++ src/main/java/com/decagon/adire/AdireApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdireApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AdireApplication.class, args);
+    public static void main(String[] args) {SpringApplication.run(AdireApplication.class, args);
     }
 
 }
